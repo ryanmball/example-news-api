76203ec4781af27530086d1e6479384b94e2d964
ryan <ball.ryan.rb@gmail.com>
2021-05-26 14:56:37 -0600

    adds news API key to credentials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.headers("X-Api-Key" => "dd1c1eb5b10b47e18b09b6f7be67ddce").get("https://newsapi.org/v2/everything?q=#{params[:search]}&sortBy=popularity")
+    response = HTTP.headers("X-Api-Key" => Rails.application.credentials.news_api_key).get("https://newsapi.org/v2/everything?q=#{params[:search]}&sortBy=popularity")
     render json: response.parse(:json)["articles"]
   end
 end
