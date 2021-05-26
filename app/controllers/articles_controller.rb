aae30482c661bed67a6c9438cbe9ef38b2a439f4
ryan <ball.ryan.rb@gmail.com>
2021-05-26 13:22:57 -0600

    adds articles index action using HTTP .headers and .get

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def index
+    response = HTTP.headers("X-Api-Key" => "API-KEY").get("https://newsapi.org/v2/everything?q=movies&from=2021-05-26&sortBy=popularity")
+    render json: response.parse(:json)["articles"][5]
+  end
 end
