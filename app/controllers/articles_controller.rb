84b46643f595076b6566bfd4c82da06d85866fc6
ryan <ball.ryan.rb@gmail.com>
2021-05-26 13:31:15 -0600

    adds search param to get request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.headers("X-Api-Key" => "API-KEY").get("https://newsapi.org/v2/everything?q=movies&from=2021-05-26&sortBy=popularity")
-    render json: response.parse(:json)["articles"][5]
+    response = HTTP.headers("X-Api-Key" => "dd1c1eb5b10b47e18b09b6f7be67ddce").get("https://newsapi.org/v2/everything?q=#{params[:search]}&sortBy=popularity")
+    render json: response.parse(:json)["articles"]
   end
 end
